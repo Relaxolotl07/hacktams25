5c2b518c42a8da56c365c85a6825474bef28b1f5
Hunter <HunterHoinkes@my.unt.edu>
2025-02-15 15:59:09 -0600

    initial commit srinikesh

---
+++ main.py
@@ -0,0 +1,30 @@
+from stockfish import Stockfish as sf
+
+
+def getBestMove():
+    stockfish = sf(
+        "C:\\Users\\Its4a\\Downloads\\stockfish-windows-x86-64\\stockfish"
+    )  # "C:\Users\Jsrin\Desktop\Gue\Stock\stockfish\stockfish-windows-x86-64.exe"
+    stockfish.set_fen_position(fenVal)
+    print(stockfish.get_best_move())
+
+
+# get fen as an input
+def getTurnFromFen(fen: str) -> str:
+    fields = fen.split()
+    # The second field represents whose turn it is
+    turn = fields[1]
+
+    return turn
+
+
+pressed = False
+
+yourColor = "w"
+
+fenVal = (
+    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"  # input("Enter FEN: ")
+)
+
+if getTurnFromFen(fenVal) == yourColor or pressed:
+    getBestMove()
