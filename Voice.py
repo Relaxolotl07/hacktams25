46f27c4f69ce6be8954c85ad11dee13b8fe03641
Srinikesh Javvaji <Jsrinikesh@gmail.com>
2025-02-16 03:00:51 -0600

    Update Voice.py

--- Voice.py
+++ Voice.py
@@ -1,5 +1,5 @@
-import pyttsx3
 from stockfish import Stockfish as sf
+import pyttsx3
 
 stockfish = sf(
     "Stock15\\stockfish\\stockfish-windows-x86-64.exe"
@@ -7,7 +7,7 @@ stockfish = sf(
 
 
 fenVal = (
-    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"  # input("Enter FEN: ")
+    "4k3/2R5/7Q/8/8/8/8/4K3 w - - 0 1"  # input("Enter FEN: ")
 )
 
 def getBestMove():
@@ -16,10 +16,35 @@ def getBestMove():
     textOut = str(stockfish.get_best_move())
 
 
+def convertMove(move: str) -> str:  
+    moveText = ""
+    
+    isCapture = stockfish.will_move_be_a_capture(move) != sf.Capture.NO_CAPTURE
+
+    currPiece = str(stockfish.get_what_is_on_square(move[0:2]))
+    currPiece = currPiece[12:]
+
+    
+    
+    if (isCapture):
+        moveText = "Capture using "
+    else:
+        moveText = "Move using "
+
+    moveText += currPiece + " from " + move[0:2] + " to " + move[2:4]
+
+    return moveText
+
+
 getBestMove()
+print(textOut)
 
+stockfish.get_board_visual()
 
-# Output
+convertedText = convertMove(textOut)
+
+print(convertedText)
+
 engine = pyttsx3.init()
-engine.say(textOut)
+engine.say(convertedText)
 engine.runAndWait()
