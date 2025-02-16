f7288d608a0d70dad0e6bbc9c1600dba941c4171
Hunter <HunterHoinkes@my.unt.edu>
2025-02-16 09:54:59 -0600

    put it into voice all together now

--- HandGestures/collect_images.py
+++ HandGestures/collect_images.py
@@ -2,7 +2,7 @@ import os
 
 import cv2
 
-DATA_DIR = './data'
+DATA_DIR = "./data"
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
@@ -15,27 +15,34 @@ for j in range(number_of_classes):
     if not os.path.exists(os.path.join(DATA_DIR, str(j))):
         os.makedirs(os.path.join(DATA_DIR, str(j)))
 
-    print('Collecting data for class {}'.format(j))
+    print("Collecting data for class {}".format(j))
 
     done = False
     while True:
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
-        cv2.putText(frame, 'Press "Q"', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
-                    cv2.LINE_AA)
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(25) == ord('q'):
+        cv2.putText(
+            frame,
+            'Press "Q"',
+            (100, 50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1.3,
+            (0, 255, 0),
+            3,
+            cv2.LINE_AA,
+        )
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(25) == ord("q"):
             break
 
     counter = 0
     while counter < dataset_size:
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
-        cv2.imshow('frame', frame)
+        cv2.imshow("frame", frame)
         cv2.waitKey(25)
-        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
+        cv2.imwrite(os.path.join(DATA_DIR, str(j), "{}.jpg".format(counter)), frame)
 
         counter += 1
-
 cap.release()
 cv2.destroyAllWindows()
--- Voice.py
+++ Voice.py
@@ -1,14 +1,28 @@
 from stockfish import Stockfish as sf
 import pyttsx3
+from IMG_to_FEN import IMG_to_FEN
+import cv2
 
-stockfish = sf(
-    "Stock15\\stockfish\\stockfish-windows-x86-64.exe"
-)
+stockfish = sf("Stock15\\stockfish\\stockfish-windows-x86-64.exe")
+
+cap = cv2.VideoCapture(0)
+
+ret, frame = cap.read()
+frame = cv2.flip(frame, 1)
+cv2.imshow("frame", frame)
+cv2.waitKey(25)
+cv2.imwrite(os.path.join(DATA_DIR, str(j), "{}.jpg".format(counter)), frame)
 
 
-fenVal = (
-    "4k3/2R5/7Q/8/8/8/8/4K3 w - - 0 1"  # input("Enter FEN: ")
-)
+cap.release()
+cv2.destroyAllWindows()
+
+img = IMG_to_FEN()
+fenVal = img.fenCodeFromImage(frame)
+
+if fenVal == "":
+    fenval = "4k3/2R5/7Q/8/8/8/8/4K3 w - - 0 1"
+
 
 def getBestMove():
     global textOut
@@ -16,17 +30,15 @@ def getBestMove():
     textOut = str(stockfish.get_best_move())
 
 
-def convertMove(move: str) -> str:  
+def convertMove(move: str) -> str:
     moveText = ""
-    
+
     isCapture = stockfish.will_move_be_a_capture(move) != sf.Capture.NO_CAPTURE
 
     currPiece = str(stockfish.get_what_is_on_square(move[0:2]))
     currPiece = currPiece[12:]
 
-    
-    
-    if (isCapture):
+    if isCapture:
         moveText = "Capture using "
     else:
         moveText = "Move using "
@@ -47,6 +59,6 @@ print(convertedText)
 
 
 engine = pyttsx3.init()
-engine.setProperty('rate', 135)
+engine.setProperty("rate", 135)
 engine.say(convertedText)
 engine.runAndWait()
